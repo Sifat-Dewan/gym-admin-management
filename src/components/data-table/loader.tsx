adbd6546199f9f51ea2e9a0dc189cc41f6ee9126
Ratul Hossain <ratulhosainbnc@gmamil.com>
2025-04-24 20:41:18 +0600

    improve dark mode table loader

--- src/components/data-table/loader.tsx
+++ src/components/data-table/loader.tsx
@@ -4,7 +4,7 @@ import "@/components/data-table/loader.css";
 
 export const Loader = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50 bg-neutral-950/5">
+    <div className="fixed inset-0 flex items-center justify-center z-50 bg-neutral-950/5 dark:bg-black/50">
       <div className="loader lg:ml-[260px]"></div>;
     </div>
   );
